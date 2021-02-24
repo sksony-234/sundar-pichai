d02a6b1b2f0026be434a2a4fe085dce1a6cc13a6
SUDHANSHU KUMAR <sksony2698@gmail.com>
2021-02-24 15:52:01 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+function role(){
+    x=document.getElementById('box');
+    x.style.display="none";
+    y=document.getElementById("role");
+    y.style.display="block";
+}
+function achiv(){
+    x=document.getElementById('box');
+    x.style.display="none";
+    y=document.getElementById("achiv");
+    y.style.display="block";
+}
+function strug(){
+    x=document.getElementById('box');
+    x.style.display="none";
+    y=document.getElementById("strug");
+    y.style.display="block";
+}
+function forward(){
+    x=document.getElementById('box');
+    x.style.display="none";
+    y=document.getElementById("forward");
+    y.style.display="block";
+}
+
+function main(){
+
+    x=document.getElementById('box');
+    x.style.display="flex";
+    y=document.getElementById("strug");
+    y.style.display="none";
+    z=document.getElementById("achiv");
+    z.style.display="none";
+    a=document.getElementById("forward");
+    a.style.display="none";
+    b=document.getElementById("role");
+    b.style.display="none";
+    
+}
